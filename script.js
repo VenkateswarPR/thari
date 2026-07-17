58224dc04c2bd177beb0a7a36a550b114087d984
Venkatesh Gupta <142140731+VenkateswarPR@users.noreply.github.com>
2026-07-17 21:16:06 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+const slides = document.querySelectorAll(".slide");
 
+let currentSlide = 0;
+
+function showSlide(index) {
+
+    slides.forEach(slide => {
+        slide.classList.remove("active");
+    });
+
+    slides[index].classList.add("active");
+}
+
+setInterval(() => {
+
+    currentSlide++;
+
+    if(currentSlide >= slides.length){
+        currentSlide = 0;
+    }
+
+    showSlide(currentSlide);
+
+},4000);
